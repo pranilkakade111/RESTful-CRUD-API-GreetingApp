bf926b1b98f5595a13cd7dd9e4e9d1d193de28d6
Pranil Kakade <77184928+pranilkakade111@users.noreply.github.com>
2021-04-17 13:49:19 +0530

    Update Server.js import  database configuration in server.js and connect to the database using mongoose.

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.use(bodyParser.urlencoded({extended:true}))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url,{
+    useNewUrlParser:true
+}).then(()=>{
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now....',err);
+    process.exit();
+});
+
 // define a simple route
 app.get('/',(req,res)=>{
     res.json({"message":"Greeting is an act of communication in which human beings intentionally make their presence known to each other, to show attention to."});
