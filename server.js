67a32bf13d30caa5893fffc1c995ea7671e203df
Pranil Kakade <77184928+pranilkakade111@users.noreply.github.com>
2021-04-20 02:27:00 +0530

    Added[Feat]:Refactor to Import Database configuration file

--- server.js
+++ server.js
@@ -11,22 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-
-// Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url,{
-    useNewUrlParser:true
-}).then(()=>{
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now....',err);
-    process.exit();
-});
+const data=require('./config/database')(app);
 
 // define a simple route
 app.get('/',(req,res)=>{
