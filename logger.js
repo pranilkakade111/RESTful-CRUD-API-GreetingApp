838ba3559f5df1401f2716c809db47ae0d8dd229
Pranil Kakade <77184928+pranilkakade111@users.noreply.github.com>
2021-04-19 10:58:31 +0530

    Added[Feat]: Added Logger File

---
+++ logger.js
@@ -0,0 +1,19 @@
+const { 
+    createLogger,
+    transports,
+    format,
+    info
+
+}=require('winston');
+require('winston-mongodb');
+const logger= createLogger({
+    transports: [
+        new transports.File({
+            filename:'info.log',
+            level:'info',
+            format: format.combine(format.timestamp(), format.json())
+        })
+    ]
+});
+
+module.exports=logger;
