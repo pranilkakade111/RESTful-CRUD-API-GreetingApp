0d43e43ed1833359b296267163a7c60f2a563d34
Pranil Kakade <77184928+pranilkakade111@users.noreply.github.com>
2021-04-30 09:55:58 +0530

    Updated[Feat]: Refactor Code For Setup MVC Architecture

--- models/greet.model.js
+++ models/greet.model.js
@@ -1,10 +1,67 @@
 const mongoose = require('mongoose');
+const { error } = require('winston');
 
 const GreetSchema = mongoose.Schema({
-    UserName: String,
-    GreetingMessage: String
+  Name: String,
+  GreetingMessage: String
 }, {
-    timestamps:true
+  timestamps: true
 });
 
-module.exports = mongoose.model('Greet',GreetSchema);
+const greetingmodel = mongoose.model('Greet', GreetSchema);
+
+class GreetingModel {
+
+  create = (greetingData , callback) => {
+    const greet = new greetingmodel(greetingData);
+    greet.save((error ,greetingResult)=>{
+        if(error){
+          callback(error, null);
+        }else {
+          callback(null ,greetingResult);
+        }
+    });
+ };
+
+  findAll = (callback)=>{
+    greetingmodel.find((error ,greetingData)=>{
+        if(error){
+          callback(error ,null);
+        }else {
+          callback(null ,greetingData);
+        }
+    });
+  };
+
+  findOne = (greetingId ,callback)=>{
+      greetingmodel.findById(greetingId ,(error ,greetingResult)=>{
+          if(error){
+            callback(error ,null);
+          }else {
+            callback(null ,greetingResult);
+          }
+      });
+  };
+
+  updateOne(messageID ,updated ,callback) {
+      greetingmodel.findByIdAndUpdate(messageID ,updated , {new: true}, (error,greetingResult)=>{
+          if (error){
+            callback(error ,null);
+          }else {
+            callback(null ,greetingResult);
+          }
+      });
+  };
+
+  delete = (greetingData ,callback) =>{
+      greetingmodel.findByIdAndDelete(greetingData ,(error,greetingResult)=>{
+          if(error){
+            callback(error ,null);
+          }else {
+            callback(null ,greetingResult);
+          }
+      });
+  };
+
+}
+module.exports = new GreetingModel;
